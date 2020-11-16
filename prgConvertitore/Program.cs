cc0cf753679c352546fc87768058ba8f8b7f9c0d
gosti <gosti@LAPTOP-RSCNUK9K>
2020-11-16 11:05:22 +0100

    numeroconvertitodadecimaleabinario

--- prgConvertitore/Program.cs
+++ prgConvertitore/Program.cs
@@ -6,7 +6,33 @@ namespace prgConvertitore
     {
         static void Main(string[] args)
         {
-            
+            int numero;
+            int resto;
+            string numeroconvertito = "";
+            int divisore;
+            Console.WriteLine("inserisci un numero");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("inserisci un divisore");
+            divisore = Convert.ToInt32(Console.ReadLine());
+            while (numero > 0)
+            {
+                resto = numero % divisore;
+                numero = numero / 2;
+                numeroconvertito = Convert.ToString(resto) + numeroconvertito;
+            }
+            Console.WriteLine($"{numeroconvertito}");
+            Console.ReadLine();
         }
     }
 }
+
+
+
+
+
+
+
+
+               
+
+
